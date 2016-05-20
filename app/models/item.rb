ceb4996b8e4ebf1e6ca674822807480d86b9212a
alexfilenko <filenkoav@gmail.com>
2016-05-20 17:33:01 +0300

    5

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
 	#attr_accessible :price, :name, :real, :weight, :description
 
-	validates :price, {numericality: {greater_than:0, allow_nil: true}}
+	validates :price, numericality: {greater_than:0, allow_nil: true}
 end
 
