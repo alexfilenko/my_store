43434e166290abb5f113612608659d946e7904b1
alexfilenko <filenkoav@gmail.com>
2016-05-22 20:01:28 +0300

    7

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,9 @@
+class ItemsController < ApplicationController
+	def index
+		@items = Item.all
+		render text: @items.map {|i| "#{i.name}: #{i.price}"}.join("<br/>")
+	end
+	def create
+		render text: "item created"
+	end
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,11 @@ class Item < ActiveRecord::Base
 	#attr_accessible :price, :name, :real, :weight, :description
 
 	validates :price, numericality: {greater_than:0, allow_nil: true}
+
+	after_initialize {puts "initialize"}
+	after_save 		 {puts "after_save"}
+	after_create	 {puts "after_create"}
+	after_update	 {}
+	after_destroy	 {}
 end
 
