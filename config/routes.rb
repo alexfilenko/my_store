c3630a55eae7f8a88f37da9374239691e0248bb9
alexfilenko <filenkoav@gmail.com>
2016-05-25 20:33:34 +0300

    9

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :items
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -50,7 +51,7 @@ Rails.application.routes.draw do
   # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
+  #      (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
 end
