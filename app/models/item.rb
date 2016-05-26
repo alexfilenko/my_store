3a7e8fbb24e1aed36bf6bd21f62ae992dc502126
alexfilenko <filenkoav@gmail.com>
2016-05-26 23:41:30 +0300

    11

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,6 @@ class Item < ActiveRecord::Base
 	#attr_accessible :price, :name, :real, :weight, :description
 
 	validates :price, numericality: {greater_than:0, allow_nil: true}
-
-	after_initialize {puts "initialize"}
-	after_save 		 {puts "after_save"}
-	after_create	 {puts "after_create"}
-	after_update	 {}
-	after_destroy	 {}
+	validates :name, presence: true
 end
 
