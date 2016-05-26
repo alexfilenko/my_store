fa47c9194712cd541cf07d9040935fc88bdafa44
alexfilenko <filenkoav@gmail.com>
2016-05-26 21:15:46 +0300

    10

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,23 @@
 class ItemsController < ApplicationController
 	def index
-		@items = Item.all
-		render text: @items.map {|i| "#{i.name}: #{i.price}"}.join("<br/>")
 	end
+
 	def create
 		render text: "item created"
 	end
+
+	def show
+		Item.where(id: params[:id]).first
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+
 end
