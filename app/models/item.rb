19afb4fcd05ac89971c47c3296b2c8f40db65292
alexfilenko <filenkoav@gmail.com>
2016-05-30 22:07:00 +0300

    23

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ActiveRecord::Base
 	validates :price, numericality: {greater_than:0, allow_nil: true}
 	validates :name, presence: true
 
-	has_many :cart, throught: :positions
+	has_many :carts, through: :positions
 	has_many :positions
 	has_many :comments, as: :commentable
 end
