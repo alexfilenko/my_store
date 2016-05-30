b187e04ab629ffce5ca2575ff135f1d2d6aab20d
alexfilenko <filenkoav@gmail.com>
2016-05-30 18:38:31 +0300

    20

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,8 @@
 class Cart < ActiveRecord::Base
 
-	belongs_to :user
+  has_many                         :positions
+  belongs_to                       :user
+  has_and_belongs_to_many          :items
+  has_many                         :items, through: :positions
 
 end
