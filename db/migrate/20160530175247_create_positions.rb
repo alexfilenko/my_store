bc5ce6799be0f112629c73733f0815ce73c1a00c
alexfilenko <filenkoav@gmail.com>
2016-05-30 20:54:31 +0300

    22

--- db/migrate/20160530175247_create_positions.rb
+++ db/migrate/20160530175247_create_positions.rb
@@ -1,7 +1,9 @@
 class CreatePositions < ActiveRecord::Migration
   def change
     create_table :positions do |t|
-
+    	t.integer :carts_id
+    	t.integer :items_id
+    	t.integer :quantity
       t.timestamps null: false
     end
   end
