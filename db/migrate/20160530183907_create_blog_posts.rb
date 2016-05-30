4664569f51d7cf9e8be9a3e39446509e94191c1a
alexfilenko <filenkoav@gmail.com>
2016-05-30 21:40:26 +0300

    23

--- db/migrate/20160530183907_create_blog_posts.rb
+++ db/migrate/20160530183907_create_blog_posts.rb
@@ -1,7 +1,8 @@
 class CreateBlogPosts < ActiveRecord::Migration
   def change
     create_table :blog_posts do |t|
-
+      t.string  :title
+      t.text	:body	
       t.timestamps null: false
     end
   end
