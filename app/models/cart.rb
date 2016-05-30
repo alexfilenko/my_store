6568defa0cc41a286325e8a4857cea82dc4851d6
alexfilenko <filenkoav@gmail.com>
2016-05-30 21:24:12 +0300

    22

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,7 @@ class Cart < ActiveRecord::Base
 
   has_many                         :positions
   belongs_to                       :user
-  has_and_belongs_to_many          :items
+  
   has_many                         :items, through: :positions
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ActiveRecord::Base
 	validates :price, numericality: {greater_than:0, allow_nil: true}
 	validates :name, presence: true
 
-	has_and_belongs_to_many   :carts
+	
 
 	has_many :positions
 end
