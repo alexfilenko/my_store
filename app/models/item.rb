3a514bf2277c22d50d60c03705c60e9286c68964
alexfilenko <filenkoav@gmail.com>
2016-06-05 14:33:59 +0300

    33

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
-	#attr_accessible :price, :name, :real, :weight, :description
+	
 
 	validates :price, numericality: {greater_than:0, allow_nil: true}
 	validates :name, presence: true
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,3 @@
 class Order < ActiveRecord::Base
-	attr_accessible :user, :user_id
 	belongs_to :user
 end
